09a24bae856e61cc182c138b24a1f6012a355e97
Bartixxx32 <mcbplay1@gmail.com>
2018-06-28 01:09:13 +0000

    init

---
+++ g36118/gta5-fivem/Start Server here.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo -
+echo FXServer esx
+echo -
+pause
+start C:\NEWESX\run.cmd +exec server.cfg
+exit
---
+++ g36118/gta5-fivem/esx.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo -
+echo FXServer esx
+echo -
+pause
+start run.cmd +exec server.cfg
+exit
